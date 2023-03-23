1c3fcf8ccb37a9b884adf491a8b39dd797dfb01b
3o14 <dldnjswn4265@gmail.com>
2023-03-23 14:21:00 +0900

    temp commit

--- react/components/screens/Qrmake.js
+++ react/components/screens/Qrmake.js
@@ -1,205 +1,259 @@
-import { StatusBar } from 'expo-status-bar';
-import React ,{useEffect,useState}from 'react';
-import {StyleSheet, Text, View ,TextInput,Button,TouchableWithoutFeedback,Keyboard,Image,TouchableOpacity} from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
-import * as ImagePicker from 'expo-image-picker';
-import axios from 'axios';
-import { useNavigation } from '@react-navigation/native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
+import axios from "axios";
+import { useNavigation } from "@react-navigation/native";
 
 const Home = (route) => {
   //const scanData = route.params.scanData;
   const navigation = useNavigation();
 
+  const [inputs, setInputs] = React.useState({
+    place: "",
+    description: "", // p_i -> description 이름 수정
+  });
 
-    const [inputs, setInputs] = React.useState({
-        place: '',
-        p_i: '',
+  const { place, description } = inputs;
+
+  const onChange = (keyvalue, e) => {
+    setInputs({
+      ...inputs,
+      [keyvalue]: e,
     });
-    
-    const {place, p_i} = inputs;
-    
-        const onChange = (keyvalue, e) => {
-            setInputs({
-                ...inputs,
-                [keyvalue]: e
-            });
-        };
+    console.log(inputs);
+  };
 
-        const deleteImage = async () =>{
-          setImageUrl(null);
-        }
+  const deleteImage = async () => {
+    setImageUrl(null);
+  };
 
-        const [imageUrl, setImageUrl] = useState('');
-        const [status, requestPermission] = ImagePicker.useMediaLibraryPermissions();
+  const [imageUrl, setImageUrl] = useState("");
+  const [status, requestPermission] = ImagePicker.useMediaLibraryPermissions();
 
-        const uploadImage = async () => {
-             if(!status?.granted) {
-                   const permission = await requestPermission();
-                  if(!permission.granted) {
-                       return null;
-                   }
-             } // 권한 확인 코드
-             const result = await ImagePicker.launchImageLibraryAsync({
-                      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-                       allowsEditing: false,
-                       qulity: 1,
-                       aspect: [1, 1]
-                   });
-                   if(result.canceled) {
-                       return null;
-                   }
-                   console.log(result);
-                   setImageUrl(result.assets[0].uri);};
-                   
-                   const postTest = async() => {
-                    try{
-                         //서버에 요청 보내기
-                         const localUri = result.assets[0].uri;
-                         const filename = localUri.split('/').pop();
-                         const match = /\.(\w+)$/.exec(filename ?? '');
-                         const type = match ? `image/${match[1]}` : `image`;
-                         const formData = new formData();
-            
-                         let dataset = {
-                            description: `${inputs.talk}`,
-                           name:`${inputs.name}`,
-                             relation:`${inputs.relation}`,
-                             contact:`${inputs.phone}`,
-                             user: null,
-                             place: `${placeID}`
-            
-                         }      
-                         formData.append("data", JSON.stringify(dataset)); // JSON 형식으로 파싱 후 추가
-                         // formData.append('photo', {uri: localUri, name: filename, type});
-             
-                          await axios({
-                             method: 'post',
-                             url:'https://www.placeqr.store/places/',
-                              headers: {
-                                'content-type': 'multipart/form-data',
-                            },
-                              data: formData
-                          })
-                        } catch (error) {
-                          console.log(error);
-                        }};
-        
+  const uploadImage = async () => {
+    if (!status?.granted) {
+      const permission = await requestPermission();
+      if (!permission.granted) {
+        return null;
+      }
+    } // 권한 확인 코드
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: false,
+      qulity: 1,
+      aspect: [1, 1],
+    });
+    if (result.canceled) {
+      return null;
+    }
+    console.log("img:", result);
+    console.log("imgrul:", imageUrl);
+    setImageUrl(result.assets[0].uri);
+  };
 
+  const postTest = async () => {
+    try {
+      //서버에 요청 보내기
+      const localUri = result.assets[0].uri;
+      console.log("localuri:", localUri);
+      const filename = localUri.split("/").pop();
+      const match = /\.(\w+)$/.exec(filename ?? "");
+      const type = match ? `image/${match[1]}` : `image`;
+      const formData = new formData();
 
+      useEffect(()=>{
+        console.log("dataset: ", dataset);
+      }
+      ,[]);
+      let dataset = {
+        description: `${inputs.talk}`,
+        name: `${inputs.name}`,
+        relation: `${inputs.relation}`,
+        contact: `${inputs.phone}`,
+        user: null,
+        place: `${placeID}`,
+      };
+      formData.append("data", JSON.stringify(dataset)); // JSON 형식으로 파싱 후 추가
+      // formData.append('photo', {uri: localUri, name: filename, type});
+
+      await axios({
+        method: "post",
+        url: "https://www.placeqr.store/places/",
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+        data: formData,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <View style={styles.container}>
       <Image
-            style={{
-                width:'100%', 
-                height:'40%', 
-                position:'absolute', 
-               
-                bottom:-55,
-            }}
-            source={require("../../assets/PQRbackIMG4.png")}
-            resizeMode="center"
-            />
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={{width:'100%',height:'100%',alignItems:'center',justifyContent:'center'}}>
+        style={{
+          width: "100%",
+          height: "40%",
+          position: "absolute",
 
-      <StatusBar style="auto" />
-      <View style={styles.container2}>
-      <TextInput 
-      style={styles.inputId}
-      placeholder="장소 이름"
-      placeholderTextColor={"gray"}
-      onChangeText={(e)=> onChange("place",e)}
-      value={place}
+          bottom: -55,
+        }}
+        source={require("../../assets/PQRbackIMG4.png")}
+        resizeMode="center"
       />
-      <TextInput 
-      style={styles.inputId2}
-      placeholder="장소 소개"
-      placeholderTextColor={"gray"}
-      onChangeText={(e)=> onChange("p_i",e)}
-      value={p_i}
-      textAlignVertical="top"
-      multiline={true}
-      />
-      <View style={styles.fixToText}>
-      <TouchableOpacity onPress={() => {uploadImage();}}>
-        <MaterialIcons name="insert-photo" size={24} color="gray" />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() =>{deleteImage();}}>
-        <MaterialIcons name="close" size={24} color="gray" />
-      </TouchableOpacity>
-      </View>
-      {imageUrl && <Image source={{uri: imageUrl}} style={{width:100, height:100,marginRight:200, marginTop:10 }}/>}
-      </View>
-      
-      <TouchableOpacity onPress={() => {navigation.navigate('Qrpage'); postTest();}}
-        //hitSlop={{ bottom:100, top:100, left:100, right:100}}
-        pressRetentionOffset={{ bottom:10, top:10, left:10, right:10}}
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View
+          style={{
+            width: "100%",
+            height: "100%",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
         >
-            <View style={{backgroundColor: '#2C2F40',padding:10, borderRadius:15,width:200, alignItems:"center",
-    justifyContent: 'center' , marginBottom:60, marginTop:30, height:60}}>
-                <Text style={{fontSize:30, color:'white'}}>생성하기</Text>
+          <StatusBar style="auto" />
+          <View style={styles.container2}>
+            <TextInput
+              style={styles.inputId}
+              placeholder="장소 이름"
+              placeholderTextColor={"gray"}
+              onChangeText={(e) => onChange("place", e)}
+              value={place}
+            />
+            <TextInput
+              style={styles.inputId2}
+              placeholder="장소 소개"
+              placeholderTextColor={"gray"}
+              onChangeText={(e) => onChange("description", e)}
+              value={description}
+              textAlignVertical="top"
+              multiline={true}
+            />
+            <View style={styles.fixToText}>
+              <TouchableOpacity
+                onPress={() => {
+                  uploadImage();
+                }}
+              >
+                <MaterialIcons name="insert-photo" size={24} color="gray" />
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => {
+                  deleteImage();
+                }}
+              >
+                <MaterialIcons name="close" size={24} color="gray" />
+              </TouchableOpacity>
             </View>
-        </TouchableOpacity>
-    </View>
-    </TouchableWithoutFeedback>
+            {imageUrl && (
+              <Image
+                source={{ uri: imageUrl }}
+                style={{
+                  width: 100,
+                  height: 100,
+                  marginRight: 200,
+                  marginTop: 10,
+                }}
+              />
+            )}
+          </View>
+
+          {/* 생성하기 */}
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("Qrpage");
+              postTest;
+            }}
+            //hitSlop={{ bottom:100, top:100, left:100, right:100}}
+            pressRetentionOffset={{ bottom: 10, top: 10, left: 10, right: 10 }}
+          >
+            <View
+              style={{
+                backgroundColor: "#2C2F40",
+                padding: 10,
+                borderRadius: 15,
+                width: 200,
+                alignItems: "center",
+                justifyContent: "center",
+                marginBottom: 60,
+                marginTop: 30,
+                height: 60,
+              }}
+            >
+              <Text style={{ fontSize: 30, color: "white" }}>생성하기</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </TouchableWithoutFeedback>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor:'white',
-        flex:1,
-        alignItems: 'center',
-    justifyContent: 'center',
-   
-      }, 
-    container2: {
-   borderWidth: 1,
-   borderColor: '#2C2F40',
-   backgroundColor: '#2C2F40',
-   borderRadius:10,
-   alignItems: 'center',
-   
-    width:350,
-    height:550,
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
-  inputId : {
-    borderRadius:15,
+  container2: {
     borderWidth: 1,
-    borderColor:'#ffffff',
-    padding:10,
-    marginTop:50,
-    marginBottom:30,
-    width:300,
-    height: 50,
-    color:'black',
-    fontSize:20,
-    backgroundColor:'white',
-  },
-  inputId2 : {
-    borderRadius:15,
-    borderWidth: 1,
-    borderColor:'#ffffff',
-    padding:10,
-    width:300,
-    height:250,
-    color:'black',
-    fontSize:20,
-    backgroundColor:'white'},
+    borderColor: "#2C2F40",
+    backgroundColor: "#2C2F40",
+    borderRadius: 10,
+    alignItems: "center",
 
-    container3 : {
-    backgroundColor: '#2C2F40',
-    alignItems: 'center',
-    justifyContent: 'center',
-    },
-    fixToText: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      marginRight:250,
-      marginTop:5,
-    },
+    width: 350,
+    height: 550,
+  },
+  inputId: {
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: "#ffffff",
+    padding: 10,
+    marginTop: 50,
+    marginBottom: 30,
+    width: 300,
+    height: 50,
+    color: "black",
+    fontSize: 20,
+    backgroundColor: "white",
+  },
+  inputId2: {
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: "#ffffff",
+    padding: 10,
+    width: 300,
+    height: 250,
+    color: "black",
+    fontSize: 20,
+    backgroundColor: "white",
+  },
+
+  container3: {
+    backgroundColor: "#2C2F40",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  fixToText: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginRight: 250,
+    marginTop: 5,
+  },
 });
 
-export default Home; 
+export default Home;
